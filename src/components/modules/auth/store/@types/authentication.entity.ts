ee912cdf4bb7f5a9b26ec8189b495fd86a33691c
JoelVR17 <vermudo.com@gmail.com>
2025-03-21 17:24:43 -0600

    feat: enhance authentication store to manage logged user state and user creation

--- src/components/modules/auth/store/@types/authentication.entity.ts
+++ src/components/modules/auth/store/@types/authentication.entity.ts
@@ -1,6 +1,10 @@
+import { User } from "@/@types/user.entity";
+
 export interface AuthenticationGlobalStore {
   address: string;
   name: string;
+  loggedUser: User | null;
+  isUserCreatedWithName: boolean;
 
   connectWalletStore: (address: string, name: string) => void;
   disconnectWalletStore: () => void;
--- src/components/modules/auth/store/slices/authentication.slice.ts
+++ src/components/modules/auth/store/slices/authentication.slice.ts
@@ -1,9 +1,15 @@
 import { StateCreator } from "zustand";
 import { AuthenticationGlobalStore } from "../@types/authentication.entity";
+import {
+  addUser,
+  getUser,
+} from "@/components/modules/auth/server/authentication.firebase";
 
 const AUTHENTICATION_ACTIONS = {
   CONNECT_WALLET: "authentication/connect",
   DISCONNECT_WALLET: "authentication/disconnect",
+  UPDATE_USER: "authentication/updateUser",
+  REMOVE_API_KEY: "authentication/removeApiKey",
 } as const;
 
 export const useGlobalAuthenticationSlice: StateCreator<
@@ -16,15 +22,42 @@ export const useGlobalAuthenticationSlice: StateCreator<
     // Stores
     address: "",
     name: "",
+    loggedUser: null,
+    isUserCreatedWithName: false,
 
     // Modifiers
     connectWalletStore: async (address: string, name: string) => {
-      set({ address, name }, false, AUTHENTICATION_ACTIONS.CONNECT_WALLET);
+      const { success, data } = await getUser({ address });
+
+      if (!success) {
+        const { success: registrationSuccess, data: userData } = await addUser({
+          address,
+        });
+
+        if (registrationSuccess) {
+          set(
+            {
+              address,
+              name,
+              loggedUser: userData,
+              isUserCreatedWithName: false,
+            },
+            false,
+            AUTHENTICATION_ACTIONS.CONNECT_WALLET
+          );
+        }
+      } else {
+        set(
+          { address, name, loggedUser: data },
+          false,
+          AUTHENTICATION_ACTIONS.CONNECT_WALLET
+        );
+      }
     },
 
     disconnectWalletStore: () =>
       set(
-        { address: "", name: "" },
+        { address: "", name: "", loggedUser: undefined },
         false,
         AUTHENTICATION_ACTIONS.DISCONNECT_WALLET
       ),
