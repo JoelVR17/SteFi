f3b94556eb1c422698c6f0877a760a0d0eed424f
armandocodecr <totovip1464@gmail.com>
2025-03-23 08:43:39 -0300

    feat: create config to deploy and initialize asset

--- src/components/modules/asset/services/asset.service.ts
+++ src/components/modules/asset/services/asset.service.ts
@@ -5,7 +5,6 @@ import { buildInvokeContractOperation } from "@/lib/stellar";
 import * as StellarSDK from "@stellar/stellar-sdk";
 import { sendTransaction } from "./helper.service";
 import { signTransaction } from "@stellar/freighter-api";
-import { useGlobalAuthenticationStore } from "../../auth/store/store";
 import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
 import { Asset } from "@/@types/asset.entity";
 
@@ -13,44 +12,44 @@ export const initializeAsset = async (
   assetProperties: Asset,
   address: string
 ): Promise<ApiResponse> => {
+  const wasmHash = process.env.NEXT_PUBLIC_WASM_HASH;
+  const contractId = process.env.NEXT_PUBLIC_CONTRACT_ID;
   try {
-    const wasmHash = process.env.WASM_HASH || "";
-
     const account = await sorobanServer.getAccount(address);
     const assetScVal = parseAsset(assetProperties);
 
     const operations = [assetScVal];
+
     const operation = buildInvokeContractOperation(
-      address,
-      wasmHash,
+      contractId!,
+      wasmHash!,
       "deploy",
       "create_asset",
-      operations
+      operations,
     );
-
+    
     let transaction = new StellarSDK.TransactionBuilder(account, {
       fee: StellarSDK.BASE_FEE,
       networkPassphrase: StellarSDK.Networks.TESTNET,
     })
       .addOperation(operation)
-      .setTimeout(30)
+      .setTimeout(60)
       .build();
-
-    const unsignedTransaction = await sorobanServer.prepareTransaction(
-      transaction
-    );
+    const unsignedTransaction = 
+      await sorobanServer.prepareTransaction(transaction);
 
     const { signedTxXdr } = await signTransaction(unsignedTransaction.toXDR(), {
       address,
       networkPassphrase: WalletNetwork.TESTNET,
     });
 
-    sendTransaction(signedTxXdr);
+    await sendTransaction(signedTxXdr);
 
     return {
       status: StellarSDK.rpc.Api.GetTransactionStatus.SUCCESS,
     };
   } catch (error: any) {
+    console.log(error)
     throw new Error(error);
   }
 };
